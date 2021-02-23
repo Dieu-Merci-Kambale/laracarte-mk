9ddcb96897bfdf66deb24b174dca0e0ff87ffe46
Dieu Merci Kambale <dieumercikambale3@gmail.com>
2021-02-23 14:43:21 +0100

    setup et ajout des pages statiques

---
+++ resources/views/layouts/partials/_footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="text-center">
+	
+		<p>
+			&copy; <?php date('Y') ?> Laracaste by <a href="">Dieu Merci Kambale </a>
+
+		</p>
+		<p>
+			<b> This app been built for learning purposes. </b>
+		</p>
+
+</footer>
---
+++ resources/views/layouts/partials/_nav.blade.php
@@ -0,0 +1,44 @@
+<nav class="navbar navbar-expand-lg static-top navbar-dark bg-dark" aria-label="Main navigation">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Laracarte</a>
+    <button class="navbar-toggler p-0 border-0" type="button" data-bs-toggle="offcanvas" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/artisan">Artisan</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-bs-toggle="dropdown" aria-expanded="false">Planète</a>
+          <ul class="dropdown-menu" aria-labelledby="dropdown01">
+            <li><a class="dropdown-item" href="laravel.com">Laravel</a></li>
+            <li><a class="dropdown-item" href="laravel.io">laravel io</a></li>
+            <li><a class="dropdown-item" href="laravelcastes.com">laravelcaste</a></li>
+            <li><a class="dropdown-item" href="larajobs.com">Larajobs</a></li>
+            <li><a class="dropdown-item" href="laravel-news.com">Laravel news</a></li>
+            <li><a class="dropdown-item" href="larachat.com">LaraChat</a></li>
+          </ul>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+      <ul class="navbar navbar-nav navbar-right">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link" href="#">Register</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
